e5d6536fef5ef92e4bf06b075ebc6bf35b7e3f65
Thomas Driessen <thomas.driessen.td@gmail.com>
2020-09-18 09:48:28 +0000

    Initial commit

---
+++ src/main/java/io/jatoms/MyBenchmark.java
@@ -0,0 +1,78 @@
+package io.jatoms;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.collections.api.map.primitive.IntObjectMap;
+import org.eclipse.collections.impl.map.mutable.primitive.IntObjectHashMap;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Fork(2)
+public class MyBenchmark {
+
+    @Param({"1000", "10000", "100000", "1000000"})
+    public int size;
+
+    private HashMap<Integer, String> jm;
+    private IntObjectHashMap<String> ec;
+
+    @Setup
+    public void setUp(){
+        jm = new HashMap<>();
+        ec = new IntObjectHashMap<>();
+        for (int i = 0; i < size; i++) {
+            jm.put(i, "String" + i);
+            ec.put(i, "String" + i);
+        }
+    }
+
+    @Benchmark
+    public IntObjectMap<String> ecPutWithPrimitives(){
+        IntObjectHashMap<String> ec = new IntObjectHashMap<>();
+        for (int i = 0; i < size; i++) {
+            ec.put(i, "Test" + i);
+        }
+        return ec;
+    }
+
+    @Benchmark
+    public Map<Integer, String> jdkPut() {
+        Map<Integer, String> jm = new HashMap<>();
+        for (int i = 0; i < size; i++) {
+            jm.put(i, "Test" + i);
+        }
+        return jm;
+    }
+
+    @Benchmark
+    public String ecGet(){
+        String result = null;
+        for (int i = 0; i < size; i++) {
+            result = ec.get(i);
+        }
+        return result;
+    }
+
+    @Benchmark
+    public String jdkGet(){
+        String result = null;
+        for (int i = 0; i < size; i++) {
+            result = jm.get(i);
+        }
+        return result;
+    }
+
+}
